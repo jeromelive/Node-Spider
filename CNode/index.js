51e040bf2507657f1276decbb0579565e9ba7b57
jerome.zhao <jerome.zhao@wolaidai.com>
2018-01-20 11:24:46 +0800

    12306车票查询

--- CNode/index.js
+++ CNode/index.js
@@ -10,16 +10,24 @@ function startSpider(url) {
 	https.get(url, (res) => {
 		console.log('状态码', res.statusCode);
 		console.log('请求头', res.headers);
-		var html = '';
+		var html = '',
+				i = 1;
 		res.setEncoding('utf-8'); // 防止中文乱码
 		res.on('data', (chunk) =>{
+			// console.log('第'+ i++ +'条')
+			// console.log(chunk);
+			fs.appendFile('./index.html', chunk, 'utf-8', (err) =>{
+				if(err) {
+					console.log(err);
+				}
+			})
 			html += chunk;
 		});
 
 		res.on('end', () => {
 			var $ = cheerio.load(html);
 			$('a.topic_title').each((idx, element) => {
-				console.log($(element).text());
+				// console.log($(element).text());
 				fs.appendFile('./data.txt', $(element).text(), 'utf-8', (err) => {
 					if(err) {
 						console.log(err);
